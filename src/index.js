bdbc1c21faefe6a55c488670a51c7bc728940c91
YoonSung Kim <opqudk@gmail.com>
2021-06-09 16:33:33 +0900

    store and reducer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add'),
     minus = document.getElementById('minus'),
     number = document.querySelector('span');
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+    return count;
 };
 
-const handleAdd = () => {
-    count++;
-    updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-    count--;
-    updateText();
-};
-
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState());
