312c6bc728b44ea5ab5b8b318bfa5982e3e1eee5
YoonSung Kim <opqudk@gmail.com>
2021-06-09 16:43:52 +0900

    actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ const add = document.getElementById('add'),
     minus = document.getElementById('minus'),
     number = document.querySelector('span');
 
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
+    if ( action.type === 'ADD' ) {
+        return ++count;
+    } else if ( action.type === 'MINUS' ) {
+        return --count;
+    }
     return count;
 };
 
 const countStore = createStore(countModifier);
 
+countStore.dispatch({type: 'ADD'});
 console.log(countStore.getState());
+
+countStore.dispatch({type: 'MINUS'});
+console.log(countStore.getState());
+
