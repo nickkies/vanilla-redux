99d86be766964cd26adbf16b74b03442c4811203
YoonSung Kim <opqudk@gmail.com>
2021-06-11 16:29:21 +0900

    connecting the store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
+import {Provider} from "react-redux/lib/alternate-renderers";
+import store from "./store";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={{store}}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
